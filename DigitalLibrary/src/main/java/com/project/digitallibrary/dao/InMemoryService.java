48cf13deed7f7c7cfe0554acdd4542ba7c188152
ajak055 <ajaybharadwaj05@gmail.com>
2021-07-19 11:08:23 +0530

    added exception for few cases

--- DigitalLibrary/src/main/java/com/project/digitallibrary/dao/InMemoryService.java
+++ DigitalLibrary/src/main/java/com/project/digitallibrary/dao/InMemoryService.java
@@ -3,6 +3,7 @@ package com.project.digitallibrary.dao;
 import com.project.digitallibrary.dto.Asset;
 import com.project.digitallibrary.dto.AssetInfo;
 import com.project.digitallibrary.dto.ConfigDetails;
+import com.project.digitallibrary.exception.InvalidIdException;
 import com.project.digitallibrary.response.Response;
 import org.springframework.stereotype.Component;
 
@@ -13,19 +14,23 @@ public class InMemoryService implements DbService{
     private static List<Asset> libraryAsset = new ArrayList<>();
     private static int count =1 ;
     static {
-        Map<String, String> prop = new HashMap<>();
-         prop.put("test", "asdf");
-        libraryAsset.add(new Asset(1,"DevelopmentBoard","microprocessordBoard", 1, "RPI", "Rack12", new AssetInfo(true), new ConfigDetails()));
+        Map<String, Object> prop = new HashMap<>();
+        prop.put("test", "asdf");
+        libraryAsset.add(new Asset(1,"DevelopmentBoard","microprocessorBoard", 1, "RPI", "Rack12", new AssetInfo(true), new ConfigDetails(prop)));
     }
 
     public Response save(Asset lib){
-        if(lib.getId()== null){
-              lib.setId(++count);
+        if(lib.getId()== null) {
+            lib.setId(++count);
+
+            libraryAsset.add(lib);
+            for (Asset asset : libraryAsset) {
+            }
+            return new Response(count, "created");
         }
-        libraryAsset.add(lib);
-        for (Asset asset: libraryAsset){
+        else{
+            throw new InvalidIdException("Invalid Id");
         }
-        return new Response(count, "created");
     }
 
     public Asset findOne(int id){
